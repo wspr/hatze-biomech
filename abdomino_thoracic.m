9cc80bc881d1d476f3ba2e6aec8319e060a25390
Will Robertson <wspr81@gmail.com>
2013-03-04 14:44:19 +1100

    add moments of inertia
    - also simplifying some summation/indexing
    - and update the coordinate plotting

--- abdomino_thoracic.m
+++ abdomino_thoracic.m
@@ -2,7 +2,7 @@
 
 clear all
 
-O1 = [1;2;3]; % origin
+O1 = [0;0;0]; % origin
 
 % For logical selection of gender:
 male   = 1;
@@ -18,12 +18,12 @@ inda = (Nt+1):N;
 % Measurements and variables:
 Y1 = nan(1,N);
 X1 = nan(1,N);
-w  = nan(1,N);
 a  = nan(1,N);
 b  = nan(1,N);
-c  = nan(1,Nt);
+w  = nan(1,N);
 
-% coefficients for lungs calcs:
+% coefficients for lungs calcs: (how to generalise?)
+c      = nan(1,Nt);
 c(1:2) = 0.8333;
 c(3:6) = 0.2;
 c(7)   = 0.4111;
@@ -112,51 +112,103 @@ w(inda) = interp1([Nt N],[Y1(Nt)/2 g],inda);
 b(inda) = Y1(inda) - w(inda);
 
 % Lungs:
-a2(indt) = a(indt).*(c(1)-c(indt));
-b2(indt) = (b(indt)-a(indt)/6).*sqrt(1-(c(indt)/c(1)).^2);
+a2 = a(indt).*(c(1)-c);
+b2 = (b(indt)-a(indt)/6).*sqrt(1-(c/c(1)).^2);
 
 
 %% Calculations
 
-v_e(indt) = pi*a(indt).*b(indt)*l/N;
-m_e(indt) = gamma_t(i_m)*v_e(indt);
-m_p(indt) = (gamma_t(i_m)-gamma_l)*8/3*a2(indt).*b2(indt)*l/N;
+v_e = pi*a(indt).*b(indt)*l/N; % volume of each thoracic disk
+v_p = 8/3*a2.*b2*l/N; % volume of lungs in each disk
 
-v_1(inda) = pi*a(inda).*w(inda)*l/2/N;
-v_2(inda) = pi*a(inda).*b(inda)*l/2/N;
-m_1(inda) = gamma_a(i_m)*v_1(inda);
-m_2(inda) = gamma_a(i_m)*v_2(inda);
+m_e = gamma_t(i_m)*v_e; % mass of thorax as if it were solid
+m_p = (gamma_t(i_m)-gamma_l)*v_p; % mass difference between thorax & lungs
+m_t = (v_e-v_p)*gamma_t(i_m); % mass of thoracic volume without lungs
+m_g = v_p*gamma_l; % mass of lungs only
+
+v_1 = pi*a(inda).*w(inda)*l/2/N;
+v_2 = pi*a(inda).*b(inda)*l/2/N;
+m_1 = gamma_a(i_m)*v_1;
+m_2 = gamma_a(i_m)*v_2;
 
 v_f = (1-i_m)*4/3*pi*r^3; % breasts (2 hemispheres)
 m_f = gamma_b*v_f;
 
-V = sum(v_e(indt))+sum(v_1(inda)+v_2(inda))+v_f;
-M = sum(m_e(indt)+m_p(indt))+sum(m_1(inda)+m_2(inda))+m_f;
-
-fprintf('Mass:   %2.3f kg\n',M)
-fprintf('Volume: %1.4f m^3\n',V)
+V = sum(v_e)+sum(v_1+v_2)+v_f;
+M = sum(m_t+m_g)+sum(m_1+m_2)+m_f;
 
 % Mass centroid:
 xc = O1(1);
 yc = O1(2)+( ...
-  sum( (m_1+m_2)*0.424.*(b(inda).^2+w(inda).^2)./Y1(inda) ) ...
+  sum( (m_1+m_2).*0.424.*(b(inda).^2+w(inda).^2)./Y1(inda) ) ...
   + m_f*(b(jj)+3/8*r) ...
   )/M;
 zc = O1(3)+( ...
-    sum( (m_t+m_lu).*(21-2*indt)*l/20 ) ...
-  + sum( (m_1+m_2).*(21-2*inda)*l/20 ) ...
+    sum( (m_t+m_g).*(21-2*indt)*l/2/N ) ...
+  + sum( (m_1+m_2).*(21-2*inda)*l/2/N ) ...
   + m_f*(l-h) ...
   )/M;
 
+% Moments of inertia:
 s = l^2/1200;
+I_x = m_e.*(b(indt).^2+s) ...
+      - m_p.*(b2.^2/5+s) ...
+      + (m_e-m_p).*(yc^2+(l*(21-2*indt)/20-zc).^2);
+Ip_x = sum(I_x) ...
+       + sum(...
+           m_1.*(0.07*w(inda).^2+s+(-0.424*w(inda)-yc).^2+(l*(21-2*inda)./20-zc).^2) ...
+         + m_2.*(0.07*b(inda).^2+s+(+0.424*b(inda)-yc).^2+(l*(21-2*inda)./20-zc).^2) ...
+       ) ...
+       + m_f*(0.2594*r^2+(l-h-zc)^2+(b(jj)+3*r/8-yc)^2);
+I2_x = Ip_x;
+     
+I_y = m_e.*(a(indt).^2+s) ...
+      - m_p.*(0.06857*a2.^2+s+(c.*a(indt)+0.4*a2).^2) ...
+      + (m_e-m_p).*(l*(21-2*indt)/20-zc).^2;
+
+I2_y = sum(I_y) + sum( ...
+         (m_1+m_2).*((a(inda).^2)/4+s+(l*(21-2*inda)./20-zc).^2) ...
+       ) + m_f*(0.4*r^2+(l-h+zc)^2+(d/2)^2);
+    
+I_z = m_e.*(a(indt).^2+b(indt).^2/4) ...
+      - m_p.*(0.06857*a2.^2+(b2.^2)/5+(c.*a(indt)+0.4*a2).^2) ...
+      + (m_e-m_p)*yc^2;
+
+I2_z = sum(I_z) + sum( ...
+         m_1.*(0.07*w(inda).^2+(a(inda).^2)/4+(-0.424*w(inda)-yc).^2) ...
+         + m_2.*(0.07*b(inda).^2+(a(inda).^2)/4+(0.424*b(inda)-yc).^2) ...
+       ) + m_f*(0.2594*r^2+(b(jj)+3*r/8-yc)^2+(d/2)^2);
+
+I_yz = (m_e-m_p).*(-yc).*(l*(21-2*indt)/20-zc);
+
+I2_yz = sum(I_yz) + sum( ...
+    (l*(21-2*inda)./20-zc).*(...
+       m_1.*(-0.424*w(inda)-yc) ...
+      +m_2.*(+0.424*b(inda)-yc)) ...
+  ) + m_f*(b(jj)+3*r/8-yc)*(l-h-zc);
+
+Ip_y = (I2_y+I2_z)/2+sqrt(((I2_y-I2_z)^2)/4+I2_yz^2);
+Ip_z = (I2_y+I2_z)/2-sqrt(((I2_y-I2_z)^2)/4+I2_yz^2);
+
+theta = atan(I2_yz/(I2_y-Ip_z));
+
+disp('-------------------------')
+disp('Abdomino-thoracic section')
+disp('-------------------------')
+fprintf('Mass:     %2.3f kg\n',M)
+fprintf('Volume:   %1.4f m^3\n',V)
+fprintf('Centroid: [ %2.0f , %2.0f , %2.0f ] mm\n',1000*xc,1000*yc,1000*zc)
+fprintf('Theta:    %2.2f°\n',theta*180/pi)
+fprintf('Moments of inertia: [ %2.3f , %2.3f , %2.3f ] kg.m^2\n',Ip_x,Ip_y,Ip_z)
 
 %% Plot
 
 hfig = figure(1); clf; hold on
 set(hfig,'color','white')
 
-plot_coord(O1,'index','1');
-plot_coord(O2,'index','2');
+% axes:
+plot_coord(O1,'index','1''');
+plot_coord(O2,'index','2''');
 
 opt  = {'opacity',0.1,'edgeopacity',0.1};
 optl = {'opacity',0.2,'edgeopacity',0.1};
@@ -185,7 +237,11 @@ if i_m == female
   plot_sphere(O1+[-d/2; b(jj); l-h],r,opt{:})
 end
 
-plot3(xc,yc,zc,'.k', 'markersize',40)
+% principle axes:
+plot_coord(O1,'index','1','axes','yz','length',0.1,'rotate',[theta*180/pi 0 0])
+
+% centroid:
+plot3(xc,yc,zc,'.k', 'markersize',10)
 
 axis equal
 view(153,23)
--- plot_coord.m
+++ plot_coord.m
@@ -5,19 +5,25 @@ function plot_coord(origin,varargin)
 % By default is aligned with the right-handed XYZ global coordinate system.
 % Use optional arguments to customise:
 %
-% KEY    VALUE    DESCRIPTION
-%
-% 'length' L      Length of axes lines
-% 'headratio' R      Ratio of head size to line length (0 < R < 1)
-% 'rotate' [u v w] Rotate coordinates 'u' degrees around the X-axis
-%                                     'v' degrees around the Y-axis
-%                                     'w' degrees around the Z-axis
-% 'labels' [true/false] Whether to print coordinate system labels
-% 'index'  str    String index on labels (default str='1')
-% 'linecolour' [R G B] Red-Green-Blue colour of axis lines
-% 'headcolour' [R G B] Red-Green-Blue colour of arrowhead faces
-% 'headopacity' C Opacity of arrowhead faces
-%
+% ------------------------------------------------------------------------
+% KEY           VALUE    DESCRIPTION
+% ------------------------------------------------------------------------
+% 'rotate'      [u v w]  Rotate coordinates 'u' degrees around the X-axis,
+%                                           'v' degrees around the Y-axis,
+%                                           'w' degrees around the Z-axis.
+% 'axes'        ['x' and/or 'y' and/or 'z']
+%                        Only plot axes listed
+% 'labels'      [true/false]
+%                        Whether to print coordinate system labels
+% 'index'       str      String index on labels (default str='1')
+% ------------------------------------------------------------------------
+% 'length'      L        Length of axes lines
+% 'headlength'  R        Length of arrow head
+% 'arrowangle'  a        Angle of arrowhead "quills"
+% 'linecolour'  [R G B]  Red-Green-Blue colour of axis lines
+% 'headcolour'  [R G B]  Red-Green-Blue colour of arrowhead faces
+% 'headopacity' C        Opacity of arrowhead faces
+% ------------------------------------------------------------------------
 
 %% Parse inputs:
 
@@ -25,18 +31,19 @@ p = inputParser;
 p.addRequired('origin');
 p.addOptional('rotate',[0 0 0]);
 p.addOptional('length',0.05);
-p.addOptional('headratio',1/3);
+p.addOptional('headlength',0.02);
 p.addOptional('arrowangle',25);
 p.addOptional('index','1');
 p.addOptional('labels',true);
-p.addParamValue('linecolour',[1 1 1]);
+p.addOptional('axes','xyz');
+p.addParamValue('linecolour',[0 0 0]);
 p.addParamValue('headcolour',0.5*[1 1 1]);
 p.addParamValue('headopacity',0.9);
 p.parse(origin,varargin{:})
 
 O      = p.Results.origin;
 al     = p.Results.length;
-aratio = p.Results.headratio;
+hl     = p.Results.headlength;
 ang    = p.Results.arrowangle;
 
 r    = p.Results.rotate;
@@ -45,17 +52,18 @@ ecol = p.Results.linecolour;
 col  = p.Results.headcolour;
 opac = p.Results.headopacity;
 labels_bool = p.Results.labels;
+plot_axes = p.Results.axes;
 
 % Constant that should actually be optional input
-nameshift = al/5;
+nameshift = 0.01;
 
 %% Definition of a single axis (X)
 %
 % This is rotated to plot the other two in Y and Z.
 
 ax = [al; 0; 0]; % axis end point
-pxy = ax - aratio*al*[cosd(ang);  sind(ang); 0]; % one point of the arrowhead
-pxz = ax - aratio*al*[cosd(ang); 0;  sind(ang)]; % one point of the other arrowhead
+pxy = ax - hl*[cosd(ang);  sind(ang); 0]; % one point of the arrowhead
+pxz = ax - hl*[cosd(ang); 0;  sind(ang)]; % one point of the other arrowhead
 head1 = [ax pxy pxy.*[1; -1; 1]]; % arrowhead points in XY plane
 head2 = [ax pxz pxz.*[1; 1; -1]]; % arrowhead points in XZ plane
 
@@ -66,39 +74,40 @@ R = Rz(r(3))*Ry(r(2))*Rx(r(1));
 
 hold on
 
-if labels_bool
-  text(-nameshift+O(1),-nameshift+O(2),O(3),['O_',ni]);
+if strcmp(plot_axes,'xyz')
+  if labels_bool
+    text(-nameshift+O(1),-nameshift+O(2),O(3),['O_',ni]);
+  end
 end
-plot_one_coord(O,R*ax,        R*head1,        R*head2,        ['x_',ni],[2*nameshift; 0; 0])
-plot_one_coord(O,R*Rz(+90)*ax,R*Rz(+90)*head1,R*Rz(+90)*head2,['y_',ni],[0; nameshift; 0])
-plot_one_coord(O,R*Ry(-90)*ax,R*Ry(-90)*head1,R*Ry(-90)*head2,['z_',ni],[0; 0; nameshift])
+
+for s = plot_axes
+  switch s
+    case 'x'
+      plot_one_coord(O,R*ax,        R*head1,        R*head2,        ['x_',ni],[2*nameshift; 0; 0])
+    case 'y'
+      plot_one_coord(O,R*Rz(+90)*ax,R*Rz(+90)*head1,R*Rz(+90)*head2,['y_',ni],[0; nameshift; 0])
+    case 'z'
+      plot_one_coord(O,R*Ry(-90)*ax,R*Ry(-90)*head1,R*Ry(-90)*head2,['z_',ni],[0; 0; nameshift])
+  end
+end
 
 %% Nested functions
 
   function plot_one_coord(O,a,head1,head2,name,ns)
-    plot_line(O,a)
     if labels_bool
       text(ns(1)+O(1)+a(1),ns(2)+O(2)+a(2),ns(3)+O(3)+a(3),name);
     end
-    patcht(O,head1)
-    patcht(O,head2)
-  end
-
-  function patcht(O,R)
-    patch(O(1)+R(1,:),O(2)+R(2,:),O(3)+R(3,:),ecol,'facealpha',opac,'facecolor',col);
+    
+    plot3(O(1)+[0 a(1)],O(2)+[0 a(2)],O(3)+[0 a(3)],'color',ecol);
+    patch(O(1)+head1(1,:),O(2)+head1(2,:),O(3)+head1(3,:),ecol,'facealpha',opac,'facecolor',col);
+    patch(O(1)+head2(1,:),O(2)+head2(2,:),O(3)+head2(3,:),ecol,'facealpha',opac,'facecolor',col);
   end
 
 end
 
-%% Sub functions
+%% Rotation matrices
 %
-% These could all also be anonymous functions.
-
-function plot_line(O,r)
-plot3(O(1)+[0 r(1)],O(2)+[0 r(2)],O(3)+[0 r(3)],'k');
-end
-
-% Rotation matrices:
+% These could all be anonymous functions if we wanted.
 
 function R = Rz(t)
 R = [cosd(t) -sind(t) 0;
@@ -117,7 +126,6 @@ R = [1 0        0      ;
      0 cosd(t) -sind(t);
      0 sind(t)  cosd(t)];
 end
-
 
 % assert( all( Rx(90)*[0;1;0]==[ 0; 0; 1]) )
 % assert( all( Rz(90)*[0;1;0]==[-1; 0; 0]) )
