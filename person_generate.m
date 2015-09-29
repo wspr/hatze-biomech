007826f429d8153c3175486fee69821da651706e
Will Robertson <wspr81@gmail.com>
2015-09-29 11:25:38 +0930

    replace deprecated matlab interp1 method

--- person_generate.m
+++ person_generate.m
@@ -434,8 +434,8 @@ orig_pos = linspace(0,l,Norig);
 meas_pos = linspace(0,l,Nmeas);
 calc_pos = linspace(0,l,Ncalc);
 
-meas = interp1(orig_pos,orig_meas,meas_pos,'cubic');
-calc = interp1(meas_pos,meas,calc_pos,'cubic');
+meas = interp1(orig_pos,orig_meas,meas_pos,'pchip');
+calc = interp1(meas_pos,meas,calc_pos,'pchip');
 
 end
 
