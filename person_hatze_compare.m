45479976215c63562f548a8053016e3db4294416
harmooni <redmoon88@gmx.net>
2017-03-02 13:41:54 +0100

    changed from degree to rad for better comparison

--- person_hatze_compare.m
+++ person_hatze_compare.m
@@ -148,10 +148,10 @@ for s = 1:person.N
       fprintf('                   ([ %3.3f , %3.3f , %3.3f ])\n',1000*person.segment(s).Minertia_hatze(1),1000*person.segment(s).Minertia_hatze(2),1000*person.segment(s).Minertia_hatze(3))
     end
     if ~isempty(person.segment(s).theta)
-      fprintf('Theta: %2.3f°\n',person.segment(s).theta*180/pi)
+      fprintf('Theta: %2.3f rad\n',person.segment(s).theta) %*180/pi)
     end
     if ~isempty(person.segment(s).theta_hatze)
-      fprintf('      (%2.3f°)\n',person.segment(s).theta_hatze*180/pi)
+      fprintf('      (%2.3f rad)\n',person.segment(s).theta_hatze) %*180/pi)
     end
 
   end
@@ -194,8 +194,8 @@ for s = 1:person.N
     if ~isempty(person.segment(s).theta)
       if abs( (person.segment(s).theta-person.segment(s).theta_hatze )/person.segment(s).theta_hatze) > thresh
         disp(['--- ',person.segment(s).name,' ---'])
-        fprintf('Theta: %2.3f°\n',person.segment(s).theta*180/pi)
-        fprintf('      (%2.3f°)\n',person.segment(s).theta_hatze*180/pi)
+        fprintf('Theta: %2.3f rad\n',person.segment(s).theta) %*180/pi)
+        fprintf('      (%2.3f rad)\n',person.segment(s).theta_hatze) %*180/pi)
       end
     end
   end
